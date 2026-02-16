78340e663555fb2ddbb68c1bb285fae3b0497059
megarex133 <binby133@gmail.com>
2026-02-16 22:55:29 +0300

    task done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,106 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+  const validated_username = users.filter((user) => user.username == username);
+  return validated_username != [];
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  //returns boolean
+  //write code to check if username and password match the one we have in records.
+  // Filter the users array for any user with the same username and password
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  // Return true if any valid user is found, otherwise false
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // Check if username or password is missing
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+  // Authenticate user
+  if (authenticatedUser(username, password)) {
+    // Generate JWT access token
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 * 200 },
+    );
+    // Store access token and username in session
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res
+      .status(208)
+      .json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let current_review = req.body.review;
+  let current_user = req.user;
+  let isbn = req.params.isbn;
+  let book = books[isbn];
+  if (book != undefined) {
+    if (book) {
+      book.reviews[current_user] = current_review;
+
+      return res
+        .status(200)
+        .send(
+          `The review for the book with ISBN ${isbn} has been added/updated by ${current_user}. ${JSON.stringify(book)}`,
+        );
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  let current_user = req.user;
+  let isbn = req.params.isbn;
+  let book = books[isbn];
+  if (book != undefined) {
+    if (book) {
+      if (book.reviews[current_user] != undefined) {
+        delete book.reviews[current_user];
+        return res
+          .status(200)
+          .send(
+            `The review for the book with ISBN ${isbn} has been removed by ${current_user}. ${JSON.stringify(book)}`,
+          );
+      } else {
+        return res
+          .status(404)
+          .json({ message: "No review found for this book" });
+      }
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  }
 });
 
 module.exports.authenticated = regd_users;
